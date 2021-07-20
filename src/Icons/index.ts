e5848df1a663b177c87c4535b3bd3d16253fc145
LerikP <lerikpav@gmail.com>
2021-07-20 17:43:10 +0300

    Move icons

--- src/Icons/index.ts
+++
@@ -1,1 +0,0 @@
-export { Ambulance, Calendar, Flasher, Home, Hospital, Stethoscope, Tooth } from './Program'
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { Ambulance, Calendar, Flasher, Home, Hospital, Stethoscope, Tooth } from './Icons'
+export { Ambulance, Calendar, Flasher, Home, Hospital, Stethoscope, Tooth } from './ServiceTypes'
