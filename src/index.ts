4a8b9d18a3343d94a596340114b51dbb45162ffd
Nikita Livyk <n.livyk@bestdoctor.ru>
2021-08-20 09:55:46 +0300

    feat: add Edit icon
    fix: removed duplicate calendar icon

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Ambulance, Calendar, Flasher, Home, Hospital, Stethoscope, Tooth } from './ServiceTypes'
+
 export * from './Common'
 export { bestDoctorIcon, letterIcons } from './FavIcons'
