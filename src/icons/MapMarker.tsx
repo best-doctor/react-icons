cb28b819734b446d86aa81bbfdabb38bee62f733
n.livyk@bestdoctor.ru <Nikita Livyk>
2021-11-04 13:23:48 +0000

    chore: add genereated icons

--- src/icons/MapMarker.tsx
+++ src/icons/MapMarker.tsx
@@ -3,20 +3,20 @@ import * as React from 'react'
 function SvgMapMarker(props: React.SVGProps<SVGSVGElement>, svgRef?: React.Ref<SVGSVGElement>) {
   return (
     <svg
-      width={24}
-      height={24}
-      viewBox="0 0 24 24"
+      width={16}
+      height={16}
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       ref={svgRef}
       {...props}
     >
       <path
-        d="M19.15 9.161c0 1.28-.443 2.743-1.167 4.26-.72 1.507-1.687 3.007-2.669 4.347A45.435 45.435 0 0112 21.77a45.435 45.435 0 01-3.314-4.002c-.982-1.34-1.95-2.84-2.669-4.347-.724-1.517-1.167-2.98-1.167-4.26C4.85 5.107 8.067 1.85 12 1.85c3.933 0 7.15 3.257 7.15 7.311z"
+        d="M12.483 6.107c0 .794-.277 1.725-.75 2.718-.47.983-1.104 1.968-1.753 2.853A30.042 30.042 0 018 14.094a30.045 30.045 0 01-1.981-2.417c-.649-.884-1.283-1.869-1.753-2.852-.474-.993-.75-1.924-.75-2.718 0-2.551 2.023-4.59 4.483-4.59 2.46 0 4.484 2.039 4.484 4.59z"
         stroke={props.color || 'currentColor'}
         strokeWidth={1.7}
       />
-      <circle cx={12} cy={9} r={2.35} stroke={props.color || 'currentColor'} strokeWidth={1.3} />
+      <circle cx={8} cy={6} r={1.35} stroke={props.color || 'currentColor'} strokeWidth={1.3} />
     </svg>
   )
 }
