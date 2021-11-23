2ef94d0951518ea1d8d3d89098ef91ea31c14154
n.livyk@bestdoctor.ru <Nikita Livyk>
2021-11-23 08:53:52 +0000

    chore: add genereated icons

--- src/icons/Calendar.tsx
+++ src/icons/Calendar.tsx
@@ -2,17 +2,9 @@ import * as React from 'react'
 
 function SvgCalendar(props: React.SVGProps<SVGSVGElement>, svgRef?: React.Ref<SVGSVGElement>) {
   return (
-    <svg
-      width={24}
-      height={24}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      ref={svgRef}
-      {...props}
-    >
+    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" ref={svgRef} {...props}>
       <path
-        d="M17 4h4a1 1 0 011 1v16a1 1 0 01-1 1H3a1 1 0 01-1-1V5a1 1 0 011-1h4V2h2v2h6V2h2v2zm-2 2H9v2H7V6H4v4h16V6h-3v2h-2V6zm5 6H4v8h16v-8z"
+        d="M14.168 2.5h3.333a.833.833 0 01.834.833v13.334a.833.833 0 01-.834.833h-15a.833.833 0 01-.833-.833V3.333a.833.833 0 01.833-.833h3.334V.833H7.5V2.5h5V.833h1.667V2.5zm-1.667 1.667h-5v1.666H5.835V4.167h-2.5V7.5h13.333V4.167h-2.5v1.666h-1.667V4.167zm4.167 5H3.335v6.666h13.333V9.167z"
         fill={props.color || 'currentColor'}
       />
     </svg>
